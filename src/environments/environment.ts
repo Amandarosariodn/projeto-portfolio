0a5c8c72eb8ed2497511aa15b47c9306409f16c3
Amandarosariodn <amandaredes15@gmail.com>
2026-06-21 18:57:07 -0300

    fix: add environment for emailjs

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+
+  emailJs: {
+    serviceId: 'service_o95azdn',
+    templateId: 'template_6x925io',
+    publicKey: '3udcXrCd6_ktncos_'
+  }
+};
